f9efacc05ca1e1043b049ec2b9cb36c8e919a4b1
anthony14khoury <anthony14khoury@gmail.com>
2022-08-08 11:46:56 -0400

    Made Navbar pretty

--- website-repo/src/components/navigation.js
+++ website-repo/src/components/navigation.js
@@ -1,15 +1,35 @@
 import './navigation.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Resume from '../assets/Anthony Khoury Resume.pdf';
 
 import * as React from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function Navigation() {
     return (
-        <nav className="nav">
-            <Link to="/" className="site-title">Home</Link>
-            <CustomLink to="/experience">Experience</CustomLink>
-            <CustomLink to="/about">About</CustomLink>
-        </nav>
+        <Navbar bg="light" variant="light">
+            <Container>
+                <Nav className="mx-auto">
+                    <Nav.Link className='nav-center'>
+                        <Link to="/" className="nav-name">Home</Link>
+                    </Nav.Link>
+
+                    <Nav.Link className='nav-center'>
+                        <CustomLink to="/about" className="nav-name">About</CustomLink>
+                    </Nav.Link>
+
+                    <Nav.Link className='nav-center'>
+                        <CustomLink to="/experience" className="nav-name">Experience</CustomLink>
+                    </Nav.Link>
+                    
+                    <Nav.Link className='nav-center'>
+                        <a className='nav-center' href={Resume} target="_blank" rel="noreferrer" class="nav-name">Resume</a>
+                    </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
 
